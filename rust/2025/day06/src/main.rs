4ad1c20c06093200297bfb84d28f4bbcd50fee24
Chris <christian.naibaf@gmail.com>
2025-12-06 19:06:08 +0100

    rust day 06 2025 part2

--- rust/2025/day06/src/main.rs
+++ rust/2025/day06/src/main.rs
@@ -6,6 +6,17 @@ fn part1(content: &str) {
     // Vec nur printbar mit {:?}  
     // println!("{:?}", lines);
 
+
+    // ohne .iter geht der print dananch nicht mehr :D
+    // der print println!("{:?}", lines); geht mit lines.iter() oder &lines
+    // iter_mnut wenn ihcdas im vektor ändern will elemnte 
+    // for line in lines.iter(){
+    //     println!("{}", line);
+    // }
+
+    // println!("{:?}", lines);
+
+
     // if lines.is_empty() { return 0; }    best pratice 
 
     // operatoren , zahlen davor 
@@ -46,9 +57,92 @@ fn part1(content: &str) {
 }
 
 
+//---------------------------
+
+fn process_group(numbers: &mut Vec<u64>, op: Option<char>) -> u64 {
+    if numbers.is_empty() { return 0; }
+    
+    let result = match op {
+        Some('+') => numbers.iter().sum::<u64>(),
+        Some('*') => numbers.iter().product::<u64>(),
+        _ => 0, 
+    };
+    
+    numbers.clear(); // Wichtig: Der Vektor wird geleert!
+    result
+}
+
+fn part2(content: &str) {
+
+    let grid: Vec<Vec<char>> = content.lines()
+            .filter(|l| !l.is_empty()) 
+            .map(|l| l.chars().collect())
+            .collect();
+    
+    // println!("{:?}", grid);
+
+    let max_len = grid.iter().map(|row| row.len()).max().unwrap_or(0);
+    
+    let height = grid.len();
+
+    let mut end_sum: u64 = 0;
+    let mut temp_promblem_numbers: Vec<u64> = Vec::new();
+    let mut current_operator = None;    // Option<char>
+
+    // // Closures oder mache das außerhalb als fn funktion nicht drinnen find es komsich hat ki als optimierung Closures das darunter vorgeschlagen 
+    // let process_group = |numbers: &mut Vec<u64>, op: Option<char>| -> u64 {
+    //     if numbers.is_empty() { return 0; }
+        
+    //     let result = match op {
+    //         Some('+') => numbers.iter().sum::<u64>(),
+    //         Some('*') => numbers.iter().product::<u64>(),
+    //         _ => 0, 
+    //     };
+        
+    //     numbers.clear(); 
+    //     result
+    // };
+
+
+    for x in (0..max_len).rev() {
+        let mut col_str = String::new();
+        for y in 0..height-1 {
+            let char_pos = grid[y].get(x).unwrap_or(&' ');
+            col_str.push(*char_pos);
+        }
+
+        if col_str.trim().is_empty() {
+            if !temp_promblem_numbers.is_empty() {
+                end_sum += process_group(&mut temp_promblem_numbers, current_operator);
+                current_operator = None;
+            }
+            continue;
+        }
+
+        if let Ok(number) = col_str.trim().parse::<u64>() {
+            temp_promblem_numbers.push(number);
+        } 
+
+        let op_char = grid[height - 1].get(x).unwrap_or(&' ');
+        if *op_char == '+' || *op_char == '*' {
+            current_operator = Some(*op_char);
+        }
+
+
+    }
+
+    if !temp_promblem_numbers.is_empty() {
+        end_sum += process_group(&mut temp_promblem_numbers, current_operator);
+    }
+
+    println!("Part 2: {}", end_sum);
+
+}
+
 
 fn main() {
     let content = fs::read_to_string("inputs/06/input.txt").expect("file?");
 
     part1(&content);
+    part2(&content);
 }
